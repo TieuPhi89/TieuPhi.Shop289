59307819b148fc5053d1a94bde0d1230dd9ffd93
TieuPhi89 <126879424+TieuPhi89@users.noreply.github.com>
2024-01-28 17:38:35 +0700

    Update api.js

--- libs/api.js
+++ libs/api.js
@@ -1,5 +1,5 @@
-const urlService = `http://localhost:8080`;
-const urlImages = `http://localhost:8080`;
+const urlService = `https://tieuphihephucvu289.onrender.com`;
+const urlImages = `https://res.cloudinary.com/djqfelq4l/image/upload/v1/images`;
 
 const getAPI=(apiName)=> {
     return new Promise((resolve, reject) => {
