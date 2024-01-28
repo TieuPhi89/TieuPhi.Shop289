c72d6b70b376e24407342038ea2282e80469af4d
TieuPhi89 <126879424+TieuPhi89@users.noreply.github.com>
2024-01-28 18:32:11 +0700

    Update api_bieu_do.js

--- quan_tri/js/api_bieu_do.js
+++ quan_tri/js/api_bieu_do.js
@@ -1,6 +1,22 @@
 const Dia_chi_Dich_vu = `https://tieuphihephucvu289.onrender.com`
 const Dia_chi_Img=`https://res.cloudinary.com/djqfelq4l/image/upload/v1/images`;
 
+const apiDangnhap=(nguoidung)=>{
+    return new Promise((Ket_qua, Loi) => {
+        let Du_lieu = {}
+        let Xu_ly_HTTP = new XMLHttpRequest()
+        Xu_ly_HTTP.onload = () => {
+            var Chuoi_JSON = Xu_ly_HTTP.responseText
+            Du_lieu = JSON.parse(Chuoi_JSON)
+            Ket_qua(Du_lieu)
+        }
+        let Tham_so = `Login`
+        let Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}/${Tham_so}`
+        Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly)
+        Xu_ly_HTTP.send(JSON.stringify(nguoidung))
+    })
+}
+
 
 function apiTivi() {
     return new Promise((resolve, reject) => {
